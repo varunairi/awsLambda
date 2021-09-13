b37efd0e96f40f3d03d45884e80bb92e97cb0661
AB98771 <AB98771@5CG6323NFY.ctl.intranet>
2021-09-13 19:37:34 +0530

    project to integrate SNS and SQS with Lambda

--- Readme.md
+++ Readme.md
@@ -9,8 +9,11 @@
 *   The "Handler" class in initialized once so all initializations can be done in constructor, static block etc and then "handleRequest" method is run .
 
 
-## Branch: environmentVariables
-Access runtime environments using System.getEnv (unencrypted)
+## Branch: readSNSAndWriteSQS
+Access runtime environments using System.getEnv (unencrypted) to get SQS Queue NAme.
+The Lambda Expects an event from SNS , processes it and then sends it over SQS.
+Lambda needs execution roles over CloudWatch(if logging), SNS and SQS.
+Next SNS needs to have a trigger to invoke the Lambda. 
 
 
 
@@ -18,6 +21,7 @@ Access runtime environments using System.getEnv (unencrypted)
 
 1.  Using AWS Management Console:
 	  Login to the aws console and in Lambda console upload the JAR file. You also have to mention the entry method "handleRequest" and fully qualified name of the class.  
+	  Also create SNS Topic and SQS Queue , then configure the sqs queue as environment variable for the Lambda
 2.  Using SAM:
 3.	Using AWS CLI: aws lambda update-function-code --function-name simple-http-endpoint --zip-file fileb://simpleawsserverless-dev-SNAPSHOT.jar --region us-east-2
 
--- src/main/java/com/varuntech/Handler.java
+++ src/main/java/com/varuntech/Handler.java
@@ -1,7 +1,7 @@
 package com.varuntech;
 
-import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
@@ -9,23 +9,63 @@ import org.apache.logging.log4j.Logger;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.varuntech.vo.Request;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent.SNSRecord;
 
-public class Handler implements RequestHandler<Request,Map<String,String> > {
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+
+public class Handler implements RequestHandler<SNSEvent,Map<String,String> > {
 
 	private static final Logger LOG=LogManager.getLogger(Handler.class);
+	private SqsClient sqsClient;
+	private  GetQueueUrlRequest getQueueRequest=null;
+	String queueName=null;
+	public Handler() {
+		super();
+		sqsClient= SqsClient.builder().region(Region.US_EAST_2).build();
+		queueName = System.getenv("sqs_name");
+		getQueueRequest = GetQueueUrlRequest.builder()
+                .queueName(queueName)
+                .build();
+	}
 
-	public Map<String,String>  handleRequest(Request  input, Context context) {
-		LOG.info("Recieved request for time : " + input.toString());
-		String db=System.getenv("db_key");
-		String environment = System.getenv("env");
+	public Map<String,String>  handleRequest(SNSEvent  input, Context context) {
+		List<SNSRecord> notificationRecords = input.getRecords();
+		LOG.info("Recieved %s events " ,notificationRecords.size());
+		
+		StringBuilder message = new StringBuilder();
+		for(SNSRecord record:notificationRecords) {
+			 message.append(record.getSNS().getMessage());
+			
+		}
+		
+		String queueUrl = sqsClient.getQueueUrl(getQueueRequest()).queueUrl();
+
+		SendMessageRequest smr = SendMessageRequest.builder().queueUrl(queueUrl)
+				.messageBody(getMessageBody(message.toString())).build();
+		
+		sqsClient.sendMessage(smr);
 		Map<String,String> responseMap = new HashMap<>();
-		responseMap.put("message:", "The time is : " + (new Date().toGMTString()));
-		responseMap.put("environment",environment);
-		responseMap.put("db",db);
+		responseMap.put("message:", "Successfully Sent Message");
 		
 		return responseMap;
 	}
 	
+	
+	private GetQueueUrlRequest getQueueRequest() {
+		if(!this.queueName.equalsIgnoreCase((String)System.getenv("sqs_name")))
+			getQueueRequest = GetQueueUrlRequest.builder()
+						        .queueName(queueName)
+						        .build();
+		return this.getQueueRequest;
+	}
+	
+	private String getMessageBody(String message) {
+		//Add body here
+		return "{\"message\":\""+message + "\"}";
+	}
 
 }
